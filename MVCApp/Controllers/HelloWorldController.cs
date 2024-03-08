82e6e5a6dbe1b164738dce296068dae860b5c060
samuelgarciah9 <samuel.garciah9@gmail.com>
2024-03-07 23:55:27 -0600

    Primer movimiento

---
+++ MVCApp/Controllers/HelloWorldController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace MVCApp.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Welcome(string name, int numTimes = 1)
+        {
+            ViewData["Message"] = "Hello " + name;
+            ViewData["NumTimes"] = numTimes;
+            return View();
+        }
+
+        //public string Index()
+        //{
+        //    return "This is my default action...";
+        //}
+
+        //Get: /HelloWorld/Wecome/
+        //Requieres using System.Text.Ecodings.Web
+        //public string Welcome(string name, int numTimes = 1)
+        //{
+        //    //return "This is the welcome action method...";
+        //    return HtmlEncoder.Default.Encode($"Hello {name}, Numtime is: {numTimes}");
+        //}
+
+
+    }
+}
